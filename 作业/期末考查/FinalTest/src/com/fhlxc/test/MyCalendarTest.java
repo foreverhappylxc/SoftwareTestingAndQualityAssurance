5aa597eab5a18fe55ad022d1f57a30892f6193c1
fhlxc <z15881614642@163.com>
2020-05-22 23:02:31 +0800

    project2分支覆盖还差一点

--- 作业/期末考查/FinalTest/src/com/fhlxc/test/MyCalendarTest.java
+++ 作业/期末考查/FinalTest/src/com/fhlxc/test/MyCalendarTest.java
@@ -32,20 +32,47 @@ public class MyCalendarTest {
     Type type;
     
     enum Type {
-        Empty
+        Empty,
+        OneSize,
+        NonEmpty,
+        Error
     }
     static List<List<Integer[]>> list = new ArrayList<List<Integer[]>>();
     
     static {
         list.add(null);
         List<Integer[]> tmp = new ArrayList<Integer[]>();
-        tmp.add(new Integer[] {});
+        tmp.add(new Integer[] {-4, 9});
+        list.add(tmp);
+        tmp = new ArrayList<Integer[]>();
+        tmp.add(new Integer[] {9, 10});
+        tmp.add(new Integer[] {-185, -100});
+        tmp.add(new Integer[] {-89, 7});
+        tmp.add(new Integer[] {13, 13});
+        tmp.add(new Integer[] {24, 28});
+        list.add(tmp);
     }
     
     @Parameters
     public static Collection<?> data() {
         return Arrays.asList(new Object[][] {
-            {Type.Empty, 10, 20, 1}
+            {Type.Empty, 10, 20, 1}, // 测试空map的时候
+            {Type.OneSize, -7, -6, 1}, // 测试位于范围左边
+            {Type.OneSize, 10, 12, 1}, // 测试位于范围右边
+            {Type.OneSize, -6, -2, 0}, // 测试下边界相交
+            {Type.OneSize, 8, 14, 0}, // 测试上边界相交
+            {Type.OneSize, -2, 7, 0}, // 测试包含
+            {Type.NonEmpty, 10, 12, 1}, // 测试一般值
+            {Type.NonEmpty, 89, 88, 0}, // 测试start小于end的情况
+            {Type.NonEmpty, 10, 12, 1}, // 测试上边界临界
+            {Type.NonEmpty, -10, 9, 1}, // 测试下边界临界
+            {Type.NonEmpty, -190, -183, 0}, // 测试下边界相交
+            {Type.NonEmpty, 27, 30, 0}, // 测试上边界相交
+            {Type.NonEmpty, 25, 26, 0}, // 测试包含
+            {Type.NonEmpty, 13, 13, 1}, // 测试特殊值
+            {Type.NonEmpty, -200, -185, 1}, // 使分支覆盖率提升 
+            {Type.NonEmpty, -200, -98, 0},
+            {Type.Error, 4, 8, 1}, //为了覆盖率百分百添加的这个用例
         });
     }
     
@@ -71,8 +98,12 @@ public class MyCalendarTest {
         switch (type) {
         case Empty -> 
             initMap(list.get(Type.Empty.ordinal()), myCalendar);
+        case NonEmpty ->
+            initMap(list.get(Type.NonEmpty.ordinal()), myCalendar);
+        case OneSize ->
+            initMap(list.get(Type.OneSize.ordinal()), myCalendar);
         default ->
-            throw new IllegalArgumentException("Unexpected value: " + type);
+            throw new IllegalArgumentException("不符合题目的类型");
         }
     }
 
@@ -84,9 +115,9 @@ public class MyCalendarTest {
     @Test
     public void testBook() {
         if (anti == 1) {
-            assertTrue(myCalendar.book(start, end));
+            assertTrue("start: " + start + " end: " + end + " 期望值是true，但是输出了false", myCalendar.book(start, end));
         } else {
-            assertFalse(myCalendar.book(start, end));
+            assertFalse("start: " + start + " end: " + end + " 期望值是false，但是输出了true", myCalendar.book(start, end));
         }
     }
 
